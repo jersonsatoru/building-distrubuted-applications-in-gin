cf593d4860b48ed7207762cb23b610763e173445
Jerson Satoru Uyekita <jersonsatoru@yahoo.com.br>
2022-02-25 14:45:10 -0300

    feat: add new recipe handler

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jersonsatoru/building-distributed-applications-in-gin
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/rs/xid v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+)
+
+var (
+	Recipes = make([]Recipe, 0)
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/", helloWorldHandler)
+	r.GET("/recipes", NewRecipeHandler)
 	r.Run()
 }
 
-func helloWorldHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "Hello World",
-	})
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	err := c.ShouldBindJSON(&recipe)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	id := xid.New().String()
+	publishedAt := time.Now()
+	recipe.ID = id
+	recipe.PublishedAt = publishedAt
+	Recipes = append(Recipes, recipe)
+	c.Writer.Header().Set("Location", fmt.Sprintf("/recipes/%s", recipe.ID))
+	c.JSON(http.StatusCreated, recipe)
+}
+
+type Recipe struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publisehdAt"`
 }
