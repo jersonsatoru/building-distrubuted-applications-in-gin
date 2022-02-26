d464dd0c35df4759e5299b3c01e19ae3251eca8b
Jerson Satoru Uyekita <jersonsatoru@yahoo.com.br>
2022-02-25 21:49:20 -0300

    feat: finished recipes CRUD

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -15,7 +17,11 @@ var (
 
 func main() {
 	r := gin.Default()
-	r.GET("/recipes", NewRecipeHandler)
+	r.POST("/recipes", NewRecipeHandler)
+	r.GET("/recipes", ListRecipesHandler)
+	r.PUT("/recipes/:id", UpdateRecipeHandler)
+	r.DELETE("/recipes/:id", DeleteRecipeHandler)
+	r.GET("/recipes/search", SearchRecipesHandler)
 	r.Run()
 }
 
@@ -32,7 +38,75 @@ func NewRecipeHandler(c *gin.Context) {
 	recipe.PublishedAt = publishedAt
 	Recipes = append(Recipes, recipe)
 	c.Writer.Header().Set("Location", fmt.Sprintf("/recipes/%s", recipe.ID))
-	c.JSON(http.StatusCreated, recipe)
+	c.JSON(http.StatusCreated, map[string]interface{}{
+		"recipe": recipe,
+	})
+}
+
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"data": Recipes,
+	})
+}
+
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Params.ByName("id")
+	registryIndex := -1
+	for index, recipe := range Recipes {
+		if recipe.ID == id {
+			registryIndex = index
+			Recipes = append(Recipes[:index], Recipes[index+1:]...)
+		}
+	}
+	if registryIndex == -1 {
+		c.JSON(http.StatusNotFound, nil)
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var recipe Recipe
+	err := c.ShouldBindJSON(&recipe)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	recipeIndex := -1
+	for index := range Recipes {
+		if id == Recipes[index].ID {
+			recipeIndex = index
+			r := &Recipes[index]
+			r.Name = recipe.Name
+			r.Ingredients = recipe.Ingredients
+			r.Tags = recipe.Tags
+			r.Instructions = recipe.Instructions
+			break
+		}
+	}
+	if recipeIndex == -1 {
+		c.Writer.WriteHeader(http.StatusNoContent)
+		return
+	}
+	c.JSON(http.StatusOK, Recipes[recipeIndex])
+}
+
+func SearchRecipesHandler(c *gin.Context) {
+	searchedTag := c.Query("tag")
+	found := []*Recipe{}
+	for _, recipe := range Recipes {
+		for _, tag := range recipe.Tags {
+			if strings.EqualFold(searchedTag, tag) {
+				found = append(found, &recipe)
+				log.Printf("%v", recipe)
+				break
+			}
+		}
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"data": found,
+	})
 }
 
 type Recipe struct {
